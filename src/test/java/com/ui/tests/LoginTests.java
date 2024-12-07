df840b8d1cbf173efff05de6652d5074b88cc28a
Sarbani <paulsarbani39@gmail.com>
2024-12-07 14:48:42 +0530

    Uncommented first login test

--- src/test/java/com/ui/tests/LoginTests.java
+++ src/test/java/com/ui/tests/LoginTests.java
@@ -9,12 +9,12 @@ import org.testng.annotations.Test;
 @Listeners({com.ui.listeners.TestListener.class})
 public final class LoginTests extends TestBase {
 
-   /* @Test(description = "Verifies if valid user is able to login into the application", groups = {"e2e","sanity"},
+   @Test(description = "Verifies if valid user is able to login into the application", groups = {"e2e","sanity"},
                         dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestDataProvider")
     public void validLoginTest(User user) {
         assertEquals(homePage.goToLogin().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
                     "Jatin Sharma","Username mismatch");
-    }*/
+    }
 
     @Test(description = "Verifies if valid user is able to login into the application", groups = {"e2e","sanity"},
             dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginCSVTestDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
